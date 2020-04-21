f8ea7f97dda08459083fbfc75462ba2ed65c2352
github username <josephblakeharwood@gmail.com>
2020-04-20 22:27:16 -0400

    removed old files

--- allstar-backend/client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import AuthProvider from './Context/AuthContext';
-import Store from './Context/Store';
-
-ReactDOM.render(<Store><AuthProvider><App /></AuthProvider></Store>, document.getElementById('root'));
